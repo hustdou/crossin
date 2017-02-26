3bab116491d838f47548a1011543f4b76b76662f
hustdou <haofeng_dou@hust.edu.cn>
2017-02-26 21:04:06 +0800

    first

---
+++ jiandan3.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+import re
+import thread
+import requests
+import os
+def getHtml(url):
+    r = requests.get(url)
+    html = r.text
+    return html
+def mkdir(path):
+    path = path.strip()
+    isExists = os.path.exists(path)
+    if not isExists:
+        os.makedirs(path)
+        return True
+    else:
+        return False
+def saveImages(html, name):
+    reg = r'src="(.+?\.jpg)"'
+    imgre = re.compile(reg)
+    imglist = imgre.findall(html)
+    for imageURL in imglist:
+        splitPath = imageURL.split('/')
+        print imageURL
+        fileName = splitPath[-1]
+        print fileName, u' 下载完成'
+        dirName = name + "/" + fileName
+        if "http:" in imageURL:
+            rr = requests.get(imageURL)
+        else:
+            rr = requests.get('http:' + imageURL)
+        data = rr.content
+        f = open(dirName, 'wb+')
+        f.write(data)
+        f.close()
+if __name__ == '__main__':
+    num1 = input('请输入起始页: ')
+    num2 = input('请输入终止页: ')
+    print u'开始下载'
+    path = u'图片'
+    mkdir(path)
+    for i in range(num1,num2+1):
+        html = getHtml("http://jandan.net/ooxx/page-%d#comments" % i)
+        thread.start_new_thread(saveImages, (html, path))
+    raw_input('press ENTER to exit...\n')
