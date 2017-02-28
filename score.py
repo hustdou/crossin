2b03fcbb38f5f4f25fdbcd4c4ef272b103b316e4
hustdou <haofeng_dou@hust.edu.cn>
2017-02-28 10:15:27 +0800

    second

--- score.py
+++ score.py
@@ -8,9 +8,19 @@ with open('report.txt','r') as f:
         list.append(list[-1]/(len(list)-1))
         list1.append(list)
     list1.sort(key=lambda student: student[10],reverse = True)
-    list1.insert(0, [0,'姓名','语文','数学','英语','物理','化学','生物','政治','历史','地理','总分','平均分'])
+    list1.insert(0, ['名次','姓名','语文','数学','英语','物理','化学','生物','政治','历史','地理','总分','平均分'])
+    list3 = ['0','平均']
+    list2 = []
+    for i in range(1,len(list1[1])):
+        for j in range(1,len(list1)):
+            list2.append(list1[j][i])
+            print j,i
+        list3.append(sum([int(i) for i in list2[:]])//len(list1))
+        del list2[:]
+    list1.insert(1,list3)
+    print list3
     j = 1
-    for i in list1[1:]:
+    for i in list1[2:]:
         i.insert(0,j)
         j += 1
         for ii in i[2:-2]:
