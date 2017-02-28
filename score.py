510dde7b5dcbe81e66dacf37bfafd567654a0257
hustdou <haofeng_dou@hust.edu.cn>
2017-02-28 12:22:39 +0800

    third

--- score.py
+++ score.py
@@ -4,8 +4,9 @@ list1= []
 with open('report.txt','r') as f:
     for content in f:
         list = content.split()
-        list.append(sum([int(i) for i in list[1:]]))
-        list.append(list[-1]/(len(list)-1))
+        list.append(sum([float(i) for i in list[1:]]))
+        ave = "%.1f" % (list[-1]/(len(list)-2))
+        list.append(ave)
         list1.append(list)
     list1.sort(key=lambda student: student[10],reverse = True)
     list1.insert(0, ['名次','姓名','语文','数学','英语','物理','化学','生物','政治','历史','地理','总分','平均分'])
@@ -14,10 +15,11 @@ with open('report.txt','r') as f:
     for i in range(1,len(list1[1])):
         for j in range(1,len(list1)):
             list2.append(list1[j][i])
-        list3.append(sum([int(i) for i in list2[:]])//(len(list2)))
+            sum1 = sum([float(m) for m in list2[:]]) / (len(list2))
+            sum1= "%.1f" % (sum1)
+        list3.append(sum1)
         del list2[:]
     list1.insert(1,list3)
-
     j = 1
     for i in list1[2:]:
         i.insert(0,j)
